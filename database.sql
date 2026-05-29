102ae30403440f7bbe2a2a0054aa94eac225ca0d
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2026-05-29 10:40:27 -0400

    codigo sql do database

--- database.sql
+++ database.sql
@@ -1,7 +1,15 @@
 create database luademel;
-use luademel;
-create table pessoa(
-id int primary key,
-nome varchar(50)
+create table luademel.pessoa(
+    id int primary key,
+    nome varchar(50)
 );
-insert into pessoa values(1, "tere") 
+insert into luademel.pessoa values (1,"tere");
+
+create table luademel.produto(
+  id INT PRIMARY KEY,
+  nome VARCHAR(300),
+  categoria VARCHAR(300),
+  preco DECIMAL(10,2),
+  data_criacao DATETIME,
+  data_modificacao DATETIME
+ );
