cf32e3046379e6a13b2114e6daebc47319176d27
X-EX01 - Calibarn <vitorluciopadilhaoliveira4@gmail.com>
2026-05-29 11:44:08 -0400

    terminar atividade de frameworks

--- src/routes.ts
+++ src/routes.ts
@@ -224,7 +224,7 @@ routes.post("/cadastro_produto_v2", async (req,res) =>{
   }
 })
 
-routes.put("/produto/:id", async(req,res)=>{
+routes.put("/produtos/:id", async(req,res)=>{
   const {id} = req.params
   let {nome, preco, categoria} = req.body
 
@@ -243,15 +243,44 @@ routes.put("/produto/:id", async(req,res)=>{
     mysqlErrorHandle.verificaErroDB()
   }
 })
-routes.put("/produtos/:id", async(req,res)=>{
+
+// routes.put("/produtos/:id", async(req,res)=>{
+//   const {id} = req.params
+//   let {nome} = req.body
+
+//   if (!nome) {
+//     return res.status(400).json({mensagem:"O campo nome é obrigatório"})
+//   }
+//   try {
+//     const [result] = await connection.execute<ResultSetHeader>("UPDATE produto SET nome = ? where id = ?", [nome,id])
+//     res.status(200).json({mensage:"Sucesso ao atualizar"})
+//   } catch (err) {
+//     const mysqlErrorHandle = new MysqlErrorHandle(err,res)
+//     mysqlErrorHandle.verificaErroDB()
+//   }
+// })
+
+
+/*
+## Exercício 2
+**Na exercício anterior, criamos juntos a rota PUT /produto/:id que atualiza um produto no banco. 
+Porém, o código que fizemos possui um problema: se o cliente não enviar todos os campos no body, 
+os campos não enviados são sobrescritos com null, apagando os dados que já estavam salvos no banco.**
+*/
+routes.put("/produto/:id", async(req,res)=>{
   const {id} = req.params
-  let {nome} = req.body
+  let {nome, preco, categoria} = req.body
 
   if (!nome) {
     return res.status(400).json({mensagem:"O campo nome é obrigatório"})
   }
+
+   preco = preco  ?? null
+   categoria = categoria ?? null
+ 
   try {
-    const [result] = await connection.execute<ResultSetHeader>("UPDATE produto SET nome = ? where id = ?", [nome,id])
+   
+    const [result] = await connection.execute<ResultSetHeader>("UPDATE produto SET nome = ?, preco = ?, categoria = ? where id = ?", [nome,preco,categoria,id])
     res.status(200).json({mensage:"Sucesso ao atualizar"})
   } catch (err) {
     const mysqlErrorHandle = new MysqlErrorHandle(err,res)
@@ -259,6 +288,68 @@ routes.put("/produtos/:id", async(req,res)=>{
   }
 })
 
+/*
+## Exercício 3
+Atualizar preço com data_modificacao automática
+Enunciado: Crie a rota PUT /produto_preco/:id.
+Recebe o id pela URL e o novo preço pelo body. 
+Além do preço, o servidor deve atualizar data_modificacao automaticamente com new Date(). 
+Retornar 404 se não encontrar, 200 se atualizar./
+*/
+routes.put("/produto_preco/:id", async(req,res)=>{
+  const {id} = req.params
+  let {nome, preco, categoria} = req.body
 
+  if (!nome) {
+    return res.status(400).json({mensagem:"O campo nome é obrigatório"})
+  }
+
+  let Novopreco = preco 
+  let Novacategoria = categoria
+  let data_modificacao = new Date()
+  try {
+    const [result] = await connection.execute<ResultSetHeader>("UPDATE produto SET nome = ?, preco = ?, categoria = ?, data_modificacao = ? where id = ?",
+      [nome,Novopreco,Novacategoria,data_modificacao ,id])
+    res.status(200).json({mensage:"Sucesso ao atualizar"})
+  } catch (err) {
+    const mysqlErrorHandle = new MysqlErrorHandle(err,res)
+    mysqlErrorHandle.verificaErroDB()
+  }
+})
+
+/*
+## Exercício 4
+
+**Crie a rota PUT /produto_completo/:id. Recebe o id pela URL e qualquer combinação de nome, preco e categoria pelo body. 
+O servidor deve buscar o produto no banco antes de atualizar — se não existir, retornar 404. 
+Para cada campo não enviado, manter o valor original do banco usando o operador ??. Atualizar também data_modificacao com new Date().
+Retornar 200 com mensagem de sucesso.**
+*/
+routes.put("/produto_completo/:id", async(req,res)=>{
+  const {id} = req.params
+  let {nome, preco, categoria} = req.body
+
+  if (!nome) {
+    return res.status(400).json({mensagem:"O campo nome é obrigatório"})
+  }
+
+  let Novopreco = preco 
+  let Novacategoria = categoria
+  let data_modificacao = new Date()
+  try {
+    const [dados, campos] = await connection.execute<ResultSetHeader>("SELECT * FROM produto where id = ?", [id])
+
+    if (dados.affectedRows == 0) {
+      return res.status(400).json({mensagem:"Deu certo"})
+    }
+    
+    const [result] = await connection.execute<ResultSetHeader>("UPDATE produto SET nome = ?, preco = ?, categoria = ?, data_modificacao = ? where id = ?",
+      [nome,Novopreco,Novacategoria,data_modificacao ,id])
+    res.status(200).json({mensage:"Sucesso ao atualizar"})
+  } catch (err) {
+    const mysqlErrorHandle = new MysqlErrorHandle(err,res)
+    mysqlErrorHandle.verificaErroDB()
+  }
+})
 
 export default routes;
